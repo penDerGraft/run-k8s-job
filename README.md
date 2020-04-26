4a25cc565f5dadeb526877fa771b10ff6224ef55
Brent Pendergraft <bpent3L@gmail.com>
2020-04-25 23:23:53 -0500

    add support for kubeconfig-file

--- README.md
+++ README.md
@@ -14,19 +14,35 @@ Some example uses might be:
 - dependency provisioning
 - database migrations
 
+## Usage
 
+```yaml
+    - uses: ./
+      with:
+        kubeconfig-file: '${{ secrets.KUBECONFIG_FILE }}'        
+        image: 'penDerGraft/integration-test-job'        
+```
 
-## Required Inputs
+### Auth Strategies
 
-- `cluster-url` the base URL for your Kubernetes cluster.
+To create a new job in a cluster, `run-k8s-job` needs credentials to authenticate against the canonical cluster endpoint. This is the IP address of the Kubernetes server that handles API access for the cluster. There are two ways to do this.
+
+#### Using `kubeconfig-file` 
+This is a base64 encoded version of the config file that is stored at `$HOME/.kube/config`. If you can access your cluster with `kubectl` this file should already have the necessary data for authentication. 
+
+#### Using `cluster-url`,  `cluster-token` and `ca-file` 
+These values are included in your kubeconfig file, but there may be cases when it's more straightforward to specifiy them directly. Note that you can omit the `ca-file` if you set `allow-insecure` to true, but this should only be used in testing situations. 
+
+### Inputs
+
+- `kubeconfig-file` a base64 encoded kubeconfig file with credentials for the cluster. This file is saved to `$HOME/.kube/config` by default. Get a base64 encoded string of the file by running `cat <path-to-file> | base64 --encode` where `<path-to-file>` is the path to your kubeconfig file. 
+- `cluster-url` the cluster endpoint for your Kubernetes cluster.
 - `cluster-token` the OAuth Bearer token for your Kubernetes cluster.
 - `image` the docker image to be run as a job. The image must be publically accessible. 
-- `ca-file` the path to the root CA certificates (in PEM format) for establishing a TLS connection to the Kubernetes server. Note: this is not is not strictly a required input, but the step will fail if one is not provided and the `disable-tls` input is not explicitly set to false. It is **highly recommended** that a CA file be specified.
-
-## Optional Inputs
+- `ca-file` the path to the root CA certificates (in PEM format) for establishing a TLS connection to the Kubernetes server. Note: the step will fail if a ca file is not provided and the `disable-tls` input is not explicitly set to false. It is **highly recommended** that a CA file be specified.
 - `job-name` prefix for the auto-generated job name in Kubernetes. Defaults to the name of the repo. 
 - `namespace` the Kubernetes namespace where the job should run. Defaults to `default`
-- `disable-tls` connect to the Kubernetes server insecurely (without TLS). Should only be used for testing purposes as it leaves the connection vulnerable to [man-in-the-middle attacks](https://en.wikipedia.org/wiki/Man-in-the-middle_attack).
+- `allow-insecure` connect to the Kubernetes server insecurely (without verifying the certificate authority). Should only be used for testing purposes as it leaves the connection vulnerable to [man-in-the-middle attacks](https://en.wikipedia.org/wiki/Man-in-the-middle_attack).
 
 
 
--- main.go
+++ main.go
@@ -2,62 +2,33 @@ package main
 
 import (
 	"context"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/sethvargo/go-githubactions"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	action := githubactions.New()
 
-	clusterURL := action.GetInput("cluster-url")
-	token := action.GetInput("cluster-token")
-	namespace := action.GetInput("namespace")
-	image := action.GetInput("image")
-	jobName := action.GetInput("job-name")
-	caFilePath := action.GetInput("ca-file")
-	tlsFlag := action.GetInput("disable-tls")
-
-	if len(clusterURL) == 0 {
-		action.Fatalf("'cluster-url' is a required input but was empty")
+	input := ActionInput{
+		kubeconfigFile: action.GetInput("kubeconfig-file"),
+		clusterURL:     action.GetInput("cluster-url"),
+		clusterToken:   action.GetInput("cluster-token"),
+		namespace:      action.GetInput("namespace"),
+		image:          action.GetInput("image"),
+		jobName:        action.GetInput("job-name"),
+		caFile:         action.GetInput("ca-file"),
+		allowInsecure:  action.GetInput("allow-insecure"),
 	}
 
-	if len(token) == 0 {
-		action.Fatalf("'cluster-token' is a required input but was empty")
-	}
+	action.Debugf("kubeconfig input %s\n", input.kubeconfigFile)
 
-	if len(image) == 0 {
-		action.Fatalf("'image' is a required input but was empty")
-	}
-
-	disableTLS, err := strconv.ParseBool(tlsFlag)
-	if err != nil {
-		action.Fatalf("'disable-tls input must be either 'true' or 'false', was %s", tlsFlag)
-	}
-
-	if !disableTLS {
-		if len(caFilePath) == 0 {
-			action.Fatalf("you must either specify the file path to the root ca or explicitly disable tls using the 'disable-tls' input")
-		}
-
-		if _, err := os.Stat(caFilePath); os.IsNotExist(err) {
-			action.Fatalf("could not locate file %s; please make sure the file is available in the runner's context", caFilePath)
-		}
-	}
-
-	config, err := clientcmd.BuildConfigFromFlags(clusterURL, "")
+	config, err := BuildK8sConfig(input)
 	if err != nil {
 		action.Fatalf("%v", err)
 	}
-
-	config.Insecure = disableTLS
-	config.CAFile = caFilePath
-	config.BearerToken = token
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
@@ -65,22 +36,22 @@ func main() {
 	}
 
 	action = action.WithFieldsMap(map[string]string{
-		"job": jobName,
+		"job": input.jobName,
 	})
 
-	runner := NewJobRunner(clientset.BatchV1().Jobs(namespace), clientset.CoreV1().Pods(namespace), 5*time.Second, action)
+	runner := NewJobRunner(clientset.BatchV1().Jobs(input.namespace), clientset.CoreV1().Pods(input.namespace), 5*time.Second, action)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Minute)
-	logs, err := runner.RunJob(ctx, jobName, namespace, image)
+	logs, err := runner.RunJob(ctx, input.jobName, input.namespace, input.image)
 	defer cancel()
 
 	if err != nil {
 		if len(logs) == 0 {
 			action.Fatalf("%v", err)
 		} else {
-			action.Fatalf("job failed - job logs:\n%s", logs)
+			action.Fatalf("job failed\njob logs:\n%s", logs)
 		}
 	}
 
-	action.Debugf("job completed successfully - job logs:\n%s", logs)
+	action.Debugf("job completed successfully\njob logs:\n%s", logs)
 }
